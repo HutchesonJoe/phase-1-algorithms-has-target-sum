096504b5572658d603a23f1b96deaf7a7f6288da
Joe <hutcheson2001@yahoo.com>
2022-02-10 18:42:46 -0500

    commit tree 6bc4465daa74132a915cbb4ff0d3733eba0a151e

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  let numIndex = 0
+  
+  for (let number of array){
+    let sumArray = array.slice(numIndex+=1).map(num => number + num)
+      for(let sum of sumArray){
+        if (sum === target){
+          return true
+        }
+      }
+  }
+  return false
 }
 
 /* 
@@ -7,18 +17,33 @@ function hasTargetSum(array, target) {
 */
 
 /* 
-  Add your pseudocode here
+  for each item in the array
+    add number to the remaining items in the array
+    compare each sum to the target number
+    if the numbers are equal
+      return true
+  
 */
 
 /*
-  Add written explanation of your solution here
-*/
+  Given an array of numbers, and another number, determine whether any pair of numbers in the array, when added together, equal the second argument/number
+  */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+
+  console.log("");
+
+  console.log("My test - expecting: false");
+  console.log("=>", hasTargetSum([2, 7, 3, 1], 1))
+
+  console.log("");
+
+  console.log("My test - expecting: true");
+  console.log("=>", hasTargetSum([0, 2, 7, 3, 1], 1))
 
   console.log("");
 
